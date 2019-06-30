aeba5d7665c12a654c4084b57bead3b6c2cc335a
Mike <zzzz@ttt.ru>
2019-06-30 18:12:30 +0300

    Added base_page.py, main_page.py, modify __init__.py

---
+++ pages/base_page.py
@@ -0,0 +1,19 @@
+#cоздаем класс
+
+class  BasePage(object):
+    #Создаем метод конструктор (__init__), он будет вызываться, когда будем создавать объект
+    def __init__(self, browser, url):# и в конструктор передаем параметры, browser, url
+        self.browser = browser # сохраняем как атрибут класса.
+        self.url = url # сохраняем, как атрибут класса.
+
+    def open_page(self): # метод открывает нужную страницу
+        self.browser.get(self.url)
+
+
+
+
+
+
+
+
+
