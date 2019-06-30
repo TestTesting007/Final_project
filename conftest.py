714bf4cd372bd63a45dd2e29a636e8aeb9f90516
Mike <zzzz@ttt.ru>
2019-06-30 16:49:35 +0300

    Added files:conftest, requirements, test_main_page, __init__

---
+++ conftest.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+
+
+import pytest
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name',action='store',default ="chrome", help="Choose browser: chrome or firefox")
+    parser.addoption('--language',action='store',default='en', help="Choose languages for browser")
+
+# Согласно пункту №5 - "Достаточно, чтобы код работал только для браузера Сhrome".r Реализовано, только для браузера Chrome
+@pytest.fixture(scope ="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_language': language})
+    if browser_name == "chrome":
+        browser=webdriver.Chrome(options=options)
+        browser.implicitly_wait(30)
+        print("Start browser Chrome")
+    elif browser_name == "firefox":
+        browser = webdriver.Firefox()
+        print("start browser firefox")
+    else:
+        print("Browser {} still is not implemented".format(browser_name))
+    yield  browser
+    print("close browser")
+    browser.quit()
